84f63ef0e9bd7ab8327a2365fe2e8209be1e5176
Nash Kleppan <nkleppan@asu.edu>
2014-02-13 19:27:23 -0700

    Is github even working?

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,8 @@
 #include<stdio.h>
 
 main(){
-/* Nash Kleppan edited this file... Twice.  Because he misunderstood the directions. */
+/* Nash Kleppan edited this file... Twice.  Because he misunderstood the directions.
+for reasons unknown to science, Vishal Mehta cannot commit this file. */
 	printf("Hello World\n");
 
 }
