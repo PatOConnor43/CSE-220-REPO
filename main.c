a1bd6000c64433f9d5e740e526a1ab4b8c8a82d1
Vishal Mehta <vrmehta1@asu.edu>
2014-02-23 17:54:05 -0800

    working shit

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
  * Patrick O Connor
  * CSE 230
  * Spring 2014 ASU */
+
 #include "common.h"
 
 int main (int argc, const char *argv[])
@@ -10,33 +11,59 @@ int main (int argc, const char *argv[])
     FILE *source_file;
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
-    
-   /* Missing Code Here */
+
+	source_file = init_lister(argv[1], source_name, date);
+
+	while(get_source_line(source_file, source_name,date))
+	{
+		//Nothing to add
+	}
+	fclose(source_file);
     return 0;
 }
+
+
+/***
+copy, open file, set date & time
+***/
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
 {
     time_t timer;
     FILE *file;
-    //Open FILE -NK
-    file=fopen(source_file_name,"r"); //Open source file
+
+   //source_name (source_file_name) = name (argv[1]);
+	int length = strlen(name);
+	strncpy(source_file_name, name, length); //source_name = argv[1]
+
+	//Associates the file pointer with our input file -PO
+	file = fopen(name, "r");
+
+
+	//This section of code sets the date and time. -PO
+	timer = time(NULL);
+	dte = ctime(&timer);
     return file;
 }
 BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
 {
     char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
-    char source_buffer[MAX_SOURCE_LINE_LENGTH];
+    char source_buffer[MAX_SOURCE_LINE_LENGTH]; //next line 
     static int line_number = 0;
-    
-    if (fgets(source_buffer,MAX_SOURCE_LINE_LENGTH,src_file) !=Null) //If a source line is found, return true. -NK
-    {
-	//To be continued
-	line_number++;//increment line number
-        return (TRUE);
+
+   // printf("%s", fgets(src_name, MAX_SOURCE_LINE_LENGTH, src_file));
+    if (fgets(source_buffer, MAX_SOURCE_LINE_LENGTH, src_file) != NULL) //check if next is not null
+    {	
+	//need source_name = src_name;	
+	
+	//need sprintf
+	int x =sprintf(print_buffer, "%d:\t%s", line_number, source_buffer);
+	//need print_line
+	print_line(print_buffer, src_name , todays_date);
+	++line_number;
+        return (TRUE); // Reads and Prints a line
     }
     else
     {
         return (FALSE);
     }
 }
-
