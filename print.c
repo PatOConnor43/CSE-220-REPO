d5a1cb34ef40e106632f29ecb33757a3b768c702
Vishal Mehta <vrmehta1@asu.edu>
2014-02-23 17:56:19 -0800

    working shit for print

--- print.c
+++ print.c
@@ -4,6 +4,9 @@
  * CSE 230
  * Spring 2014 ASU */
 #include "common.h"
+#include <stdio.h>
+//I took out some of your includes Nash because they were giving me compile errors :/ -PO
+
 
 static void print_page_header(char source_name[], char date[]);
 
@@ -15,28 +18,33 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     
     if (++line_count > MAX_LINES_PER_PAGE)
     {
-        /* Missing Code Here */
+        print_page_header(source_name_to_print,date_to_print);
+	line_count = 1;
     }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
-    {
-        save_chp = line + MAX_PRINT_LINE_LENGTH;
-        save_ch = *save_chp;
-        *save_chp = '\0';
-    }
-    if (save_chp)
-    {
-        printf("%s", line);
-        *save_chp = save_ch;
-    }
-    /* Missing Code Here */
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
+	else
+	{
+		fputs(line, stdout);
+		//++line_count;
+	}
 }
+
+
 static void print_page_header(char source_name[], char date[])
 {
-    static int page_number = 0;
+    static int page_number = 1;
+    char *string = {"Page\t"};
+    char *name = {source_name};
+    char *tab= {"\t"};
+    time_t timer = time(NULL);
+	strncpy(date, ctime(&timer), DATE_STRING_LENGTH);
+	char *location = {"/Users/bholto/Downloads/"};
     
-    /* Missing Code Here */
+    //Print header. IE: "Page    1  /Users/bholto/Downloads/NEWTON.PAS  Tue Feb 18 20:56:59 2014"
+    //-NK
+    printf("%s",string);
+    printf("%d\t", page_number);
+    printf("%s%s\t", location, name);
+    printf("%s\t",date);
+    printf("%s\n", "");
+    printf("%s\n", "");
 }
