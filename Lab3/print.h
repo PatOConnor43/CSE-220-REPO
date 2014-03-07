148fff234d6b8a0e64988061070bb1c5c4bb86c6
Nash <nkleppan@asu.edu>
2014-03-07 08:17:47 -0700

    I set up the source code skeleton for Lab3 with our comment block. The design document is available on Google Docs to start working on as well. -Nash Kleppan

---
+++ Lab3/print.h
@@ -0,0 +1,21 @@
+/* Nash Kleppan
+ * Vishal Mehta
+ * Patrick O Connor
+ * CSE 230
+ * Spring 2014 ASU */
+//  print.h
+//  Lab3
+//
+//  Created by Bryce Holton.
+//  Copyright (c) 2014 Bryce Holton. All rights reserved.
+//
+
+#ifndef Lab3_print_h
+#define Lab3_print_h
+
+#include "common.h"
+
+void print_line(char *, char source_name[], char date_to_print[]);
+void print_token(Token *token);
+
+#endif
