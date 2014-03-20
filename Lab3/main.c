6d0297e3aa8004ace2f4f44d21fef8c32855fe84
Vishal Mehta <vrmehta1@asu.edu>
2014-03-20 04:53:36 -0700

    Update main.c

--- Lab3/main.c
+++ Lab3/main.c
@@ -22,9 +22,8 @@ void add_token_to_list(Token *list, Token *new_token);
 
 int main(int argc, const char * argv[])
 {
-    Token l;
-    Token *token = (Token*)malloc(sizeof(Token));
-    Token *token_list = &l; //This needs to be implemented as a linked list in scanner.h.
+    Token *token;
+   Token *token_list; //This needs to be implemented as a linked list in scanner.h.
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
     FILE *source_file = init_lister(argv[1], source_name, date);
@@ -34,64 +33,34 @@ int main(int argc, const char * argv[])
     {
         token = get_token();
         //add_token_to_list(token_list, token);
-        
+      print_token(token);
     }
-    while (token_list->next != NULL);//What is the sentinal value that ends this loop?
+    while (token != NULL);//What is the sentinal value that ends this loop?
     
     quit_scanner(source_file, token_list);
-    free(token); //Deallocate memory
     return 0;
 }
 void add_token_to_list(Token *list, Token *new_token)
 {
-    // Add new_token to the list knowing that list is a linked list.
-	static int iterations = 0;
-    //puts(new_token->literal_value);	
-	//puts("zzzzzzzzzHHHHAAALLLOOO");
 	
-	if(iterations == 0)
+	if(list == NULL)
 	{
-		//puts("HHHHAAALLLOOO");
-		list->literal_value = new_token->literal_value;
-		list->token_code = new_token->token_code;
-		list->next = NULL;
-		
-		print_token(list);
+		list = new_token;
 		
 	}
 	else
-	{
-		//puts("WHAT UP");	
-	   // while(list->next != NULL)
-		//{
-			//puts("IT WENT THROUGH WHILE");
-			//printf("%s\n", new_token->literal_value);		
-			//printf("%s\n", list->literal_value);
-			list->next = new_token;
-			//list->literal_value = new_token->literal_value;
-			//list->token_code = new_token->token_code;
-			//list->next = NULL;
-			print_token(new_token);
-			//puts(list->literal_value);
-		//}
-		//list->next = new_token;
-		//puts("Made it through while loop");
+	{	
+	    while(list->next != NULL)
+		{
+			list = list->next;
+		}
+		list->next = new_token;
 	}
-	++iterations;
-	//puts("SUCCESS");
 }
 void quit_scanner(FILE *src_file, Token *list)
 {
     //write code to free all of the memory for the token list
-    Token *temp;
-    temp= list;
-    while(temp->next != NULL) //free ALL of the linked list.
-    {
-      temp=list;
-      list=list->next;
-      free(temp);
-    }
-      
+	free(list);
     fclose(src_file);
 }
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
@@ -105,4 +74,3 @@ FILE *init_lister(const char *name, char source_file_name[], char dte[])
     strcpy(dte, asctime(localtime(&timer)));
     return file;
 }
-
