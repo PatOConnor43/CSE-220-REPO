4bb87ba88019af0df0bd6a0a46bff263a703f3c9
Vishal Mehta <vrmehta1@asu.edu>
2014-03-21 21:01:26 -0700

    Update main.c

--- Lab3/main.c
+++ Lab3/main.c
@@ -18,12 +18,12 @@
 
 FILE *init_lister(const char *name, char source_file_name[], char dte[]);
 void quit_scanner(FILE *src_file, Token *list);
-void add_token_to_list(Token *list, Token *new_token);
+void add_token_to_list(Token **list, Token *new_token);
 
 int main(int argc, const char * argv[])
 {
     Token *token;
-   Token *token_list; //This needs to be implemented as a linked list in scanner.h.
+   //Token *token_list; //This needs to be implemented as a linked list in scanner.h.
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
     FILE *source_file = init_lister(argv[1], source_name, date);
@@ -34,30 +34,34 @@ int main(int argc, const char * argv[])
         token = get_token();
         //add_token_to_list(token_list, token);
         puts("_______________--------------------------________________-------------");
-      //print_token(token);
+      print_token(token);
     }
     while (token != NULL);//What is the sentinal value that ends this loop?
     
-   // quit_scanner(source_file, token_list);
+    quit_scanner(source_file, token_list);
     return 0;
 }
-void add_token_to_list(Token *list, Token *new_token)
+void add_token_to_list(Token **list, Token *new_token)
 {
 	Token *temp=(Token*)malloc(sizeof(Token));
-	if(list == NULL)
+	if(*list == NULL)
 	{
-		list = new_token;
-		
+		puts("IT WENT NULL");
+		*list = new_token;
 	}
 	else
 	{	
-	    temp=list;
+	    temp=*list;
+	  
 	    while(temp->next != NULL)
 		{
+			//puts("IT DID NOT GO NULLLL");
 			temp = temp->next;
 		}
 		temp->next = new_token;
+		puts(temp->literal_value);
 	}
+}
 void quit_scanner(FILE *src_file, Token *list)
 {
     //write code to free all of the memory for the token list
