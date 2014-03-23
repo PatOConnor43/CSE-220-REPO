7aa30e6fdc36085a8cf761771aac7d9493752151
Nkleppan <nkleppan@asu.edu>
2014-03-22 18:01:58 -0700

    Most Recent fixes

--- Lab3/print.c
+++ Lab3/print.c
@@ -1,13 +1,13 @@
 /* Nash Kleppan
- * Vishal Mehta
- * Patrick O Connor
- * CSE 230
- * Spring 2014 ASU */
-//  print.c
-//  Lab3
+* Vishal Mehta
+* Patrick O Connor
+* CSE 230
+* Spring 2014 ASU */
+// print.c
+// Lab3
 //
-//  Created by Bryce Holton.
-//  Copyright (c) 2014 Bryce Holton. All rights reserved.
+// Created by Bryce Holton.
+// Copyright (c) 2014 Bryce Holton. All rights reserved.
 //
 #include <stdlib.h>
 #include "print.h"
@@ -39,7 +39,7 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
         print_page_header(source_name_to_print, date_to_print);
         line_count = 1;
     }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
+    if (strlen(line) > MAX_PRINT_LINE_LENGTH)
     {
         save_chp = &line[MAX_PRINT_LINE_LENGTH];
     }
@@ -62,65 +62,45 @@ static void print_page_header(char source_name[], char date[])
     char location[256];
     getcwd(location,sizeof(location));
     putchar(FORM_FEED_CHAR);
-    printf("Page    %d  %s  %s\n\n", ++page_number, location, date);
+    printf("Page %d %s %s\n\n", ++page_number, location, date);
 }
 
 
 void print_token(Token *token)
 {
     //Missing code goes here
-	char* concatenate_string[MAX_PRINT_LINE_LENGTH];// = (char*)malloc(sizeof(char));
-	char* start = "  >> ";
-	
-	while(token != NULL )
-	{
-		
-		if(token->token_code == NO_TOKEN) //SPECIAL CHARACTERS
-		{
-			if((token->next)->token_code == NO_TOKEN){
-				sprintf((char*)concatenate_string, "%1s%s%s\t\t\t%s%s", start, token->literal_value, (token->next)->literal_value, token->literal_value, (token->next)->literal_value);
-				token = token->next;
-			}
+  char* concatenate_string[MAX_PRINT_LINE_LENGTH];// = (char*)malloc(sizeof(char));
+  char* start = " >> ";
 
-			else if((token->next)->token_code != NO_TOKEN){
-			sprintf((char*)concatenate_string, "%1s%s\t\t\t%s", start, token->literal_value, token->literal_value);
-			}
-		}
-		else if(token->token_code == NUMBER )
-		{
-			sprintf((char*)concatenate_string, "%1s%s\t\t%s", start, SYMBOL_STRINGS[NUMBER], token->literal_value);
-		}
-		else if(token->token_code == STRING){
-			sprintf((char*)concatenate_string, "%1s%s\t\t%s", start, SYMBOL_STRINGS[STRING], token->literal_value);
-		}
-		else // if it is a word
-		{
-			
-			if(token->token_code >= AND && token->token_code <= WITH) //if it is a reserved word
-			{	
-				char upperCase[strlen(token->literal_value)];//= (char*) malloc(sizeof(char));
-				int i;
-				for(i = 0; i < strlen(token->literal_value); ++i){
-					upperCase[i] = toupper(token->literal_value[i]);
-					
-				}
-				upperCase[strlen(token->literal_value)] = '\0';
-				//puts(token->literal_value);
-				sprintf((char*)concatenate_string, "%s%1s\t\t%s",start, upperCase, token->literal_value);
-				
-				//free(upperCase);
-				
-			}
-			else //non-reserved word
-			{
-				sprintf((char*)concatenate_string, "%s%1s\t%s", start, SYMBOL_STRINGS[token->token_code], token->literal_value);
-			}
-		}		
-		//concatenate_string[strlen(concatenate_string)] = '\0';
-		puts((char*)concatenate_string); //print stuff
-		token = token->next; //go to next word
-		
-		//free(concatenate_string);
-	}//end while
-
+  if(token != NULL )
+  {
+    if(token->token_code == NO_TOKEN) //SPECIAL CHARACTERS
+    {
+      sprintf((char*)concatenate_string, "%1s%18s", token->literal_value, token->literal_value);
+    }
+    if(token->token_code == NUMBER )
+    {
+      sprintf((char*)concatenate_string, "%1s%18s", SYMBOL_STRINGS[NUMBER], token->literal_value);
+    }
+    else // if it is a word
+    {
+      if(token->token_code >= AND && token->token_code <= WITH) //if it is a reserved word
+      {	
+      char upperCase[strlen(token->literal_value)];//= (char*) malloc(sizeof(char));
+      int i;
+      for(i = 0; i < strlen(token->literal_value); ++i)
+      {
+	upperCase[i] = toupper(token->literal_value[i]);
+      }
+      upperCase[strlen(token->literal_value)] = '\0';
+      sprintf((char*)concatenate_string, "%s%1s\t\t%s",start, upperCase, token->literal_value);
+      }
+      else //non-reserved word
+      {
+	sprintf((char*)concatenate_string, "%s%1s\t%s", start, SYMBOL_STRINGS[token->token_code], token->literal_value);
+      }
+    }	
+      puts((char*)concatenate_string); //print stuff
+      token = token->next; //go to next word
+    }//end while
 }//end print_token
